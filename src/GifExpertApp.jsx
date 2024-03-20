c7ba11c7e5440f9302b520dedf4f5918a62f1349
Luis Garcia <lalejandro.1988@gmail.com>
2024-03-19 20:15:20 -0600

    Configuracion base Gif app

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+      {/* Titulo */}
+      <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gifs */}
+        {/* Gif Item */}
+    </>
+  )
+}
